14d50e9ae91c491faf17d64988fc5683e77ea27a
ponpon <sheep0406@hotmail.com>
2016-05-31 14:26:04 +0800

    set production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  
+
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port:           587,
@@ -14,8 +14,8 @@ Rails.application.configure do
   }
 
   config.cache_classes = true
-  config.action_mailer.default_url_options = { host: 'ponpon-artstore.herokuapp.com' }
-  
+  config.action_mailer.default_url_options = { host: 'ponpon-shoestore.herokuapp.com' }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
