d822d964b1e161014555c582e2d426131d04bb68
ponpon <sheep0406@hotmail.com>
2016-05-31 12:57:10 +0800

    set carrier_waveconfig.srotrage into fog/aws

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   if Rails.env.production?
-    config.storage :fog
+    config.storage = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
       aws_access_key_id:     'AKIAI6EJGMLW2ZTYN6OA',      # 你的 key
