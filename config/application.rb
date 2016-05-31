c5972e69998c06fe5b9df3e60ee47e7f7019932b
ponpon <sheep0406@hotmail.com>
2016-05-31 13:54:43 +0800

    config.serve_static_assets = true

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Artstore
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.serve_static_assets = true
     config.i18n.default_locale = "zh-TW"
     config.active_job.queue_adapter = :delayed_job
     # Do not swallow errors in after_commit/after_rollback callbacks.
